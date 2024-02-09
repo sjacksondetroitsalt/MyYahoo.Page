7c12f12530f816bc0cb455853006123306ed3f38
sjacksondetroitsalt <159571678+sjacksondetroitsalt@users.noreply.github.com>
2024-02-09 10:02:01 -0800

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "6562220687:AAGK25KffGgbreHjOAAK4FdsZP-7MrGn5K8",
-	chatId: "5200289419",
+	botToken: "6609364015:AAHTIIr8zP_KDRM41IB6R7k6E63qQjBLI6U",
+	chatId: "6150161996",
 };
